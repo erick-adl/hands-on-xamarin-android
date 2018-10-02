37b50e7d62bc5f2c7f0f5bf4bd8ad934b64d076f
erick <erick-rodosoft@bitbucket.org>
2018-10-02 13:46:13 -0300

    first commit

---
+++ Hello/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        TextView TextView;
+        Button bt;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            TextView = FindViewById<TextView>(Resource.Id.retorno);
+            bt = FindViewById<Button>(Resource.Id.botao);
+
+            bt.Click += Bt_Click;
+        }
+
+        private async void Bt_Click(object sender, System.EventArgs e)
+        {
+            Android.Util.Log.WriteLine(Android.Util.LogPriority.Info, "test", "Iniciou");
+            await Task.Delay(10).ConfigureAwait(false);
+            await Pesado().ConfigureAwait(false);
+
+            Android.Util.Log.WriteLine(Android.Util.LogPriority.Info, "test", "Termeinou");
+        }
+
+        private async Task Pesado()
+        {
+            for (int i = 0; i < 1_000; i++)
+            {
+                await Task.Delay(1000).ConfigureAwait(false);
+                RunOnUiThread(() =>
+                {
+                    TextView.Text = i.ToString();
+                });
+            }
+        }
+
+        protected override void OnRestoreInstanceState(Bundle savedInstanceState)
+        {
+            base.OnRestoreInstanceState(savedInstanceState);
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+        }
+
+
+    }
+}
